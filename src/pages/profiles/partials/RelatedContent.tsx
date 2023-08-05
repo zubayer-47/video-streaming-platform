1898cfdf13797923dde1c9e3a586aedc8859701f
A B M Zubayer <zubayerabm47@gmail.com>
2023-08-05 22:13:53 +0600

    done it

--- src/pages/profiles/partials/RelatedContent.tsx
+++ src/pages/profiles/partials/RelatedContent.tsx
@@ -1,10 +1,20 @@
-import img from '../../../assets/demo.jpg';
-import ThumbnailImg from "../../../components/ThumbnailImg";
+import demoImg from '../../../assets/demo.jpg';
+import ProfileThumbnail from '../../../components/ProfileThumbnail';
 
 export default function RelatedContent() {
     return (
         <div className="w-96 flex justify-center gap-2">
-            <ThumbnailImg demoImg={img} isList />
+            <ProfileThumbnail
+                thumbnail={demoImg}
+                views='7.6M'
+                timetamp='05:44'
+                title='LEADERSHIP LAB: The Craft of Writing Effectively UChicago Social Sciences The Craft of Writing Effectivel'
+                channelName={'Standford Graduate School of Business'}
+                chnLink='/channel123'
+                vidLink='/video1234'
+                uploadedAt='1month'
+                isList
+            />
         </div>
     )
 }
