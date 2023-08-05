4053f35a87ce9192c5cf8aed0fe497045169b74d
A B M Zubayer <zubayerabm47@gmail.com>
2023-08-05 22:14:17 +0600

    working on video profile

--- src/pages/profiles/VideoProfile.tsx
+++ src/pages/profiles/VideoProfile.tsx
@@ -1,12 +1,11 @@
-import { useParams } from "react-router-dom";
 import ContentBio from "./partials/ContentBio";
 import RelatedContent from "./partials/RelatedContent";
 
 export default function VideoProfile() {
-    const { videoID } = useParams();
+    // const { videoID } = useParams();
 
     return (
-        <div className="w-full px-10 lg:px-24 flex flex-col lg:flex-row gap-4">
+        <div className="w-full h-full overflow-auto px-10 lg:px-24 flex flex-col lg:flex-row gap-4">
             <ContentBio />
             <RelatedContent />
         </div>
