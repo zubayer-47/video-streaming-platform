081c63e95a683fa6a90034a590bc9f2b22d4fd59
A B M Zubayer <zubayerabm47@gmail.com>
2023-08-06 14:27:09 +0600

    sidebar modfiied

--- src/pages/videoDashboard/partials/Sidebar.tsx
+++ src/pages/videoDashboard/partials/Sidebar.tsx
@@ -2,13 +2,12 @@ import { PiHouseFill } from 'react-icons/pi';
 
 export default function Sidebar() {
 	return (
-		<div className='hidden md:w-1/6 h-[92vh] md:flex flex-col overflow-y-scroll bg-indigo-100/40'>
-			{new Array(30).fill(true).map(() => (
+		<div className='hidden md:w-3/12 lg:w-3/12 xl:w-2/12 pt-20 md:flex flex-col h-screen overflow-y-auto bg-indigo-100/40'>
+			{new Array(40).fill(true).map(() => (
 				<button
 					type='button'
 					className='flex items-center gap-4 px-3 py-2 font-[500] hover:bg-indigo-200'
 				>
-					{' '}
 					<PiHouseFill className='w-6 h-6' /> <span>Home</span>
 				</button>
 			))}
