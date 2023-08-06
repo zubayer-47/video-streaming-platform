23a86944cff84965dedcc1662227c3d302433e48
A B M Zubayer <zubayerabm47@gmail.com>
2023-08-06 14:18:44 +0600

    modified for login

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,50 @@
 import { useState } from 'react';
 import { FaCircleUser } from 'react-icons/fa6';
 import { Link } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
+import useModal from '../hooks/useModal';
+import FollowButton from './Buttons/FollowButton';
 import UserModal from './Modals/UserModal';
 import SearchBar from './SearchBar';
 
 const Header = () => {
 	const [showUserModal, setShowUserModal] = useState(false);
-
 	const handleModal = () => setShowUserModal((prev) => !prev);
+	const userContext = useAuth()
+	const modalContext = useModal()
+
 	return (
-		<div className='z-40 bg-indigo-50 flex flex-col justify-center pb-2'>
-			<div className='grid grid-cols-12 gap-3 p-2'>
-				<Link to={'/'} className='col-span-4 '>
+		<div className='z-40 bg-indigo-50 flex flex-col justify-center pb-2 mx-5'>
+			<div className='grid grid-cols-3 gap-3 p-2'>
+				<Link to={'/'}>
 					<span>Cloned YouTube</span>
 				</Link>
-				<div className='col-span-4 text-center'>
+				<div className='text-center'>
 					<SearchBar />
 				</div>
-				<div className='col-span-4 relative'>
+				<div className='text-right'>
 					{/* <FiUser className="w-8 h-8" /> */}
-					<button type='button' onClick={handleModal} className='w-full'>
-						<FaCircleUser className='w-8 h-8 absolute right-0 top-1' />
-					</button>
+					{!userContext.state.isLoggedIn ? (
+						<div className="inline-block">
+							<FollowButton
+								title='Log In'
+								handler={() => modalContext.dispatch({ type: "UPDATE_LOGIN_MODAL", payload: true })}
+								icon={<FaCircleUser className='w-6 h-6 fill-slate-500' />}
+								classes='py-2.5 text-sm'
+							/>
+						</div>
+					) : (
+						<>
+							<div className="relative inline-block">
+								<button type='button' onClick={handleModal}>
+									<FaCircleUser className='w-8 h-8 absolute right-0 top-1' />
+								</button>
 
-					{!showUserModal ? null : (
-						<UserModal isOpen={showUserModal} setOpen={setShowUserModal} />
+								{!showUserModal ? null : (
+									<UserModal isOpen={showUserModal} setOpen={setShowUserModal} />
+								)}
+							</div>
+						</>
 					)}
 				</div>
 			</div>
