a6f67885bf1e6e1c6f37a1b46c70e61c6c780a2f
A B M Zubayer <zubayerabm47@gmail.com>
2023-08-06 14:18:53 +0600

    modifying

--- src/components/ModalViews/ModalBox.tsx
+++ src/components/ModalViews/ModalBox.tsx
@@ -5,9 +5,9 @@ export default function ModalBox({ children, onClose }: ModalBoxProps) {
     return (
         <>
             <div
-                className="flex justify-center items-center overflow-y-auto fixed inset-0 outline-none z-20 focus:outline-none mx-5"
+                className="flex bg-red-300 max-w-3xl max-h-[80vh] my-auto mx-auto justify-center items-center overflow-y-auto fixed inset-0 outline-none z-20 focus:outline-none px-5"
             >
-                <button type="button" onClick={onClose} className="fixed inset-0 bg-black/25"></button>
+                <button type="button" onClick={onClose} className="fixed inset-0 bg-black/20"></button>
                 <div className="relative my-6 mx-auto">
                     {children}
                 </div>
