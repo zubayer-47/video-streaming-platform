dd90aeb583283ebf72076bb1f6c11554750b0866
A B M Zubayer <zubayerabm47@gmail.com>
2023-08-07 21:24:08 +0600

    video dashboard modified

--- src/pages/videoDashboard/VideoDashboard.tsx
+++ src/pages/videoDashboard/VideoDashboard.tsx
@@ -1,5 +1,8 @@
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import useAuth from '../../hooks/useAuth';
+import useModal from '../../hooks/useModal';
+import AuthModal from '../authModals/AuthModal';
 import Sidebar from './partials/Sidebar';
 import Tags from './partials/Tags';
 import VideoContent from './partials/VideoContent';
@@ -7,6 +10,9 @@ import VideoContent from './partials/VideoContent';
 dayjs.extend(relativeTime);
 
 export default function VideoDashboard() {
+	const userContext = useAuth()
+	const modalContext = useModal()
+
 	return (
 		<div className='fixed inset-0 flex flex-col'>
 			<div className='flex gap-3'>
@@ -18,6 +24,8 @@ export default function VideoDashboard() {
 					</div>
 				</div>
 			</div>
+
+			{!userContext.state.isLoggedIn && modalContext.state.isVisibleAuthModal ? <AuthModal /> : null}
 		</div>
 	);
 }
