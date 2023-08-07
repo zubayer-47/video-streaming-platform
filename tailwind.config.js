089e4d3d7c40ab9c5c8dcaaa43b68c6f1175efaf
A B M Zubayer <zubayerabm47@gmail.com>
2023-08-07 15:05:53 +0600

    add a few spacing for height

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ export default {
 	important: true,
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			spacing: {
+				'102': '30rem',
+				'104': '34rem',
+				'106': '38rem',
+				'110': '43rem',
+			}
+		},
 	},
 	plugins: [],
 };
