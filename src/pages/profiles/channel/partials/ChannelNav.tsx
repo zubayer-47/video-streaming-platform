ed902c34897c6bffdeefd530ce52d4fc0e980dbb
A B M Zubayer <zubayerabm47@gmail.com>
2023-08-17 00:20:11 +0600

    modifying

--- src/pages/profiles/channel/partials/ChannelNav.tsx
+++ src/pages/profiles/channel/partials/ChannelNav.tsx
@@ -1,10 +1,13 @@
-import { FC } from 'react';
+import { FC, useRef } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import { NavLink } from 'react-router-dom';
 
 interface ChannelNavProps { }
 
 const ChannelNav: FC<ChannelNavProps> = () => {
+
+    const divRef = useRef<HTMLDivElement>(null)
+
     return <ul className='flex items-center ml-14 mt-7 gap-10'>
         <NavLink to={`featured`} className={({ isActive }) => (
             !isActive ? "text-gray-700/90 border-b-2 border-transparent px-5 py-2 font-medium" : "text-gray-800 border-b-2 border-gray-700 bg-gray-700/10 font-medium px-5 py-2"
