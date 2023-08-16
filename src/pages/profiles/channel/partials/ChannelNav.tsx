b1fe06af03f2c95d0d07faddcfea7856a921ebff
A B M Zubayer <zubayerabm47@gmail.com>
2023-08-16 21:28:11 +0600

    channel nav modified

--- src/pages/profiles/channel/partials/ChannelNav.tsx
+++ src/pages/profiles/channel/partials/ChannelNav.tsx
@@ -6,24 +6,25 @@ interface ChannelNavProps { }
 
 const ChannelNav: FC<ChannelNavProps> = () => {
     return <ul className='flex items-center ml-14 mt-7 gap-10'>
-        <NavLink to='/@stacklearner2' className={({ isActive }) => (
+        <NavLink to={`featured`} className={({ isActive }) => (
             !isActive ? "text-gray-700/90 border-b-2 border-transparent px-5 py-2 font-medium" : "text-gray-800 border-b-2 border-gray-700 bg-gray-700/10 font-medium px-5 py-2"
         )}>HOME</NavLink>
-        <NavLink to='/@stacklearner3' className={({ isActive }) => (
+        <NavLink to={`videos`} className={({ isActive }) => (
             !isActive ? "text-gray-700/90 border-b-2 border-transparent px-5 py-2 font-medium" : "text-gray-800 border-b-2 border-gray-700 bg-gray-700/10 font-medium px-5 py-2"
         )}>VIDEOS</NavLink>
-        <NavLink to='/@stacklearner4' className={({ isActive }) => (
+        <NavLink to={`playlists`} className={({ isActive }) => (
             !isActive ? "text-gray-700/90 border-b-2 border-transparent px-5 py-2 font-medium" : "text-gray-800 border-b-2 border-gray-700 bg-gray-700/10 font-medium px-5 py-2"
         )}>PLAYLIST</NavLink>
-        <NavLink to='/@stacklearner5' className={({ isActive }) => (
+        <NavLink to={`channels`} className={({ isActive }) => (
             !isActive ? "text-gray-700/90 border-b-2 border-transparent px-5 py-2 font-medium" : "text-gray-800 border-b-2 border-gray-700 bg-gray-700/10 font-medium px-5 py-2"
         )}>CHANNELS</NavLink>
-        <NavLink to='/@stacklearner6' className={({ isActive }) => (
+        <NavLink to={`about`} className={({ isActive }) => (
             !isActive ? "text-gray-700/90 border-b-2 border-transparent px-5 py-2 font-medium" : "text-gray-800 border-b-2 border-gray-700 bg-gray-700/10 font-medium px-5 py-2"
         )}>ABOUT</NavLink>
-        <NavLink to='/@stacklearner7' className={({ isActive }) => (
+        <NavLink to={`@stacklearner7`} className={({ isActive }) => (
             !isActive ? "text-gray-700/90 border-b-2 border-transparent px-5 py-2 font-medium" : "text-gray-800 border-b-2 border-gray-700 bg-gray-700/10 font-medium px-5 py-2"
         )}><FiSearch /></NavLink>
     </ul>
 }
+
 export default ChannelNav
