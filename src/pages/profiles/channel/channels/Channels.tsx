3d9eb8d427d24b632001a6abc4abe8bd440256f1
A B M Zubayer <zubayerabm47@gmail.com>
2023-08-16 21:29:20 +0600

    channels section created in channel profile

---
+++ src/pages/profiles/channel/channels/Channels.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+import ChannelNavLayout from '../../../../components/Layouts/ChannelNavLayout';
+
+interface ChannelsProps { }
+
+const Channels: FC<ChannelsProps> = () => {
+    return <ChannelNavLayout>
+        <div>Channels</div>
+    </ChannelNavLayout>
+}
+export default Channels
