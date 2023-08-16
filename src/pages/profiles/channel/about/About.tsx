59c2b3e71a633a59a3c25b06ed493c8474517282
A B M Zubayer <zubayerabm47@gmail.com>
2023-08-16 21:28:56 +0600

    created about section

---
+++ src/pages/profiles/channel/about/About.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+import ChannelNavLayout from '../../../../components/Layouts/ChannelNavLayout';
+
+interface AboutProps { }
+
+const About: FC<AboutProps> = () => {
+    return <ChannelNavLayout>
+        <div>About</div>
+    </ChannelNavLayout>
+}
+export default About
