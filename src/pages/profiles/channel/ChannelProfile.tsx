4bb456e9446a25f1202c775ae802d8bef52f111e
A B M Zubayer <zubayerabm47@gmail.com>
2023-08-16 21:27:55 +0600

    channel profile modified

--- src/pages/profiles/channel/ChannelProfile.tsx
+++ src/pages/profiles/channel/ChannelProfile.tsx
@@ -1,7 +1,6 @@
 import { FC } from 'react';
-import { BsDot } from 'react-icons/bs';
 import { FiChevronRight } from 'react-icons/fi';
-import { Link, useParams } from 'react-router-dom';
+import { Link, Outlet, useParams } from 'react-router-dom';
 import demoImg from '../../../assets/demo.jpg';
 import FollowButton from '../../../components/Buttons/FollowButton';
 import ChannelNav from './partials/ChannelNav';
@@ -14,7 +13,7 @@ const ChannelProfile: FC<ChannelProfileProps> = () => {
     console.log(params)
 
     return (
-        <div className=''>
+        <div className='overflow-auto pb-5'>
             <img className='h-64 w-full object-cover' src={demoImg} alt="" />
 
             <div className='max-w-7xl mx-auto mt-7'>
@@ -45,32 +44,12 @@ const ChannelProfile: FC<ChannelProfileProps> = () => {
             <div className='border-b border-gray-300'>
                 <div className="max-w-7xl mx-auto">
                     <ChannelNav />
-
-
                 </div>
             </div>
-
-            <div className="max-w-7xl mx-auto flex gap-5 mt-5">
-                <img className='h-52 w-96 object-cover rounded-2xl' src={demoImg} alt="" />
-
-                <div className='max-w-xl'>
-                    <h1 className='line-clamp-1 font-medium '>Think in a Redux way - On demand video only React Redux course by Learn with Sumit</h1>
-
-                    <p className='my-2 text-gray-700/90 text-sm'>
-                        <span>5,002 views</span>
-                        <BsDot className='inline-block' />
-                        <span>3 weeks ago</span>
-                    </p>
-
-                    <p className='line-clamp-6 text-sm'>
-                        It's all about teaching web development skills and techniques in an efficient and practical manner. If you are just getting started in web development, "Learn with Sumit" has all the tools you need to learn the newest and most popular technologies to convert you from a no stack to full stack developer. "Learn with Sumit" also deep dives into advanced topics using the latest best practices for you seasoned web developers.
-
-                        "Learn with Sumit" was founded by Sumit Saha who is a Tech Entrepreneur also Founded Analyzen - the first ever Digital Agency in Bangladesh. He is specialized in Full Stack Web Development. Sumit started "Learn with Sumit" in order to share his passion for web development, and do what he truly loves - teach and inspire new web developers. Over the years, he has taught many colleagues and friends the joys of web development, and cannot wait to teach you.
-                    </p>
-                </div>
-            </div>
+            <Outlet />
         </div>
     )
 }
+// https://youtu.be/i6dNQFdVuLM?t=502
 
 export default ChannelProfile;
