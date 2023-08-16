902c57c2db6a534dde8461c765c26cd02e641003
A B M Zubayer <zubayerabm47@gmail.com>
2023-08-16 21:27:17 +0600

    added a few routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import { Outlet, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import PageLayout from './components/Layouts/PageLayout';
 import ChannelProfile from './pages/profiles/channel/ChannelProfile';
+import About from './pages/profiles/channel/about/About';
+import Channels from './pages/profiles/channel/channels/Channels';
+import Featured from './pages/profiles/channel/featured/Featured';
+import Playlists from './pages/profiles/channel/playlists/Playlists';
+import Video from './pages/profiles/channel/videos/Videos';
 import VideoProfile from './pages/profiles/video/VideoProfile';
 import VideoDashboard from './pages/videoDashboard/VideoDashboard';
 
@@ -13,7 +18,13 @@ function App() {
 				<Route path='/' element={<Outlet />}>
 					<Route index element={<VideoDashboard />} />
 					<Route path='watch/:videoID' element={<VideoProfile />} />
-					<Route path=':channelName' element={<ChannelProfile />} />
+					<Route path=':channelName' element={<ChannelProfile />} >
+						<Route path='featured' element={<Featured />} />
+						<Route path='videos' element={<Video />} />
+						<Route path='playlists' element={<Playlists />} />
+						<Route path='channels' element={<Channels />} />
+						<Route path='about' element={<About />} />
+					</Route>
 
 					{/* <Route element={<Protected />}>
           <Route path='register' element={<RegisterPage />} />
