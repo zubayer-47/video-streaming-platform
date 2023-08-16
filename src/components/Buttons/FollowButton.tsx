bb5c878ee134c8cf113895a06e30fee60ec00e41
A B M Zubayer <zubayerabm47@gmail.com>
2023-08-16 14:02:23 +0600

    change button theme

--- src/components/Buttons/FollowButton.tsx
+++ src/components/Buttons/FollowButton.tsx
@@ -11,6 +11,7 @@ type BtnProp = {
     classes?: string;
     icon?: ReactNode;
 };
+
 const FollowButton = ({
     title,
     handler,
@@ -23,14 +24,14 @@ const FollowButton = ({
     return (
         <button
             type={type}
-            className={`rounded-full outline-none tracking-wider bg-indigo-200/50 hover:bg-indigo-200/90 font-semibold ${!icon ? "px-6" : "flex items-center gap-2 px-3"} ${isDisabled && 'opacity-50'} ${classes}`}
+            className={`rounded-full outline-none tracking-wider text-gray-50 bg-gray-900 hover:bg-gray-800 font-semibold ${!icon ? "px-6" : "flex items-center gap-2 px-3"} ${isDisabled && 'opacity-50'} ${classes}`}
             onClick={handler}
             disabled={isLoading || isDisabled}
         >
             {!icon ? null : icon}
 
             <span className='flex items-center'>
-                <span>{title}</span>
+                <span className='text-sm md:text-md'>{title}</span>
                 {!isLoading ? null : (
                     <FiRefreshCw className='ml-2 w-5 h-5 stroke-2 text-white animate-spin' />
                 )}
