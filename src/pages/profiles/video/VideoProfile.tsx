de56a3473afe6aed48c740968e9a8ad86bcfe56b
A B M Zubayer <zubayerabm47@gmail.com>
2023-08-17 23:00:49 +0600

    modified

--- src/pages/profiles/video/VideoProfile.tsx
+++ src/pages/profiles/video/VideoProfile.tsx
@@ -1,13 +1,13 @@
-import ContentBio from "./partials/ContentBio";
-import RelatedContent from "./partials/RelatedContent";
+import RelatedContent from './partials/RelatedContent';
+import VideoBody from './partials/VideoBody';
 
 export default function VideoProfile() {
     // const { videoID } = useParams();
 
     return (
-        <div className="w-full h-full overflow-auto px-2 lg:px-10 xl:px-24 flex flex-col lg:flex-row gap-7 py-2">
-            <ContentBio />
+        <div className='w-full h-full overflow-auto px-2 lg:px-10 xl:px-24 flex flex-col lg:flex-row gap-7 py-2'>
+            <VideoBody />
             <RelatedContent />
         </div>
-    )
+    );
 }
