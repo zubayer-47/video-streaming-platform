bac4285df7d81efbf0e69e63734ed6b07c2df40f
A B M Zubayer <zubayerabm47@gmail.com>
2023-08-17 23:02:00 +0600

    add upload video page

---
+++ src/pages/profiles/channel/upload/UploadVideo.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+interface UploadVideoProps { }
+
+const UploadVideo: FC<UploadVideoProps> = () => {
+    return <div>UploadVideo</div>
+}
+
+export default UploadVideo;
