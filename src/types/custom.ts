a3804ca1f78ad3127de71b1bfc0b79abb67c893b
A B M Zubayer <zubayerabm47@gmail.com>
2023-08-18 22:45:18 +0600

    modifie

--- src/types/custom.ts
+++ src/types/custom.ts
@@ -3,3 +3,4 @@ import React from "react";
 export type InputType = React.ChangeEvent<HTMLInputElement>;
 export type ClickType = React.MouseEventHandler<HTMLInputElement>;
 export type FormHandler = React.FormEventHandler<HTMLFormElement>;
+export type BooleanSetStateType = React.Dispatch<React.SetStateAction<boolean>>;
