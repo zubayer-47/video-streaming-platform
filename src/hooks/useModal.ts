74321f6aa007064775ae1356422fed387d9526ee
A B M Zubayer <zubayerabm47@gmail.com>
2023-08-18 22:33:31 +0600

    modified

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,9 +1,17 @@
-import { useContext } from "react";
+import { useCallback, useContext } from "react";
 import { ModalContext } from "../contexts/modal/Provider";
 
 export default function useModal() {
   const context = useContext(ModalContext);
-  if (!context) throw new Error("useModal must be used within a ModalProvider");
+  if (!context) throw new Error("useAuth must be used within a ModalProvider");
 
-  return { ...context };
+  const { dispatch } = context;
+
+  const updateModal = useCallback(
+    (payload: boolean) => {
+      dispatch({ type: "UPDATE_AUTH_MODAL", payload });
+    },
+    [dispatch]
+  );
+  return { ...context, updateModal };
 }
