b234f531ce4fdee6ac6cc6248ab28b4c72d6734d
A B M Zubayer <zubayerabm47@gmail.com>
2023-08-19 23:04:55 +0600

    replaced copy version of VideoBody component

--- src/pages/profiles/video/VideoProfile.tsx
+++ src/pages/profiles/video/VideoProfile.tsx
@@ -1,12 +1,12 @@
 import RelatedContent from './partials/RelatedContent';
-import VideoBody from './partials/VideoBody';
+import VideoBodyCopy from './partials/VideoBody(copy)';
 
 export default function VideoProfile() {
     // const { videoID } = useParams();
 
     return (
         <div className='w-full h-full overflow-auto px-2 lg:px-10 xl:px-24 flex flex-col lg:flex-row gap-7 py-2'>
-            <VideoBody />
+            <VideoBodyCopy />
             <RelatedContent />
         </div>
     );
