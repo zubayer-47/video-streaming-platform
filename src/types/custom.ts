46f6310a737dfa4393e037d162a71e3ce7971114
A B M Zubayer <zubayerabm47@gmail.com>
2023-08-20 23:04:28 +0600

    add button handler

--- src/types/custom.ts
+++ src/types/custom.ts
@@ -2,5 +2,6 @@ import React from "react";
 
 export type InputType = React.ChangeEvent<HTMLInputElement>;
 export type ClickType = React.MouseEventHandler<HTMLInputElement>;
+export type ButtonClickHandler = React.MouseEventHandler<HTMLButtonElement>;
 export type FormHandler = React.FormEventHandler<HTMLFormElement>;
 export type BooleanSetStateType = React.Dispatch<React.SetStateAction<boolean>>;
