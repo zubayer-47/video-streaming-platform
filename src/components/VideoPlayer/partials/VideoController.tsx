f17262cc0968ac6dacb852e09af7ec3766dcdfd0
Rasel Developer <dev.code.get@gmail.com>
2023-08-20 06:35:15 +0600

    Working controller settings ....

--- src/components/VideoPlayer/partials/VideoController.tsx
+++ src/components/VideoPlayer/partials/VideoController.tsx
@@ -1,7 +1,10 @@
 import {
+	// FiChevronRight,
 	FiMaximize,
 	FiPause,
 	FiPlay,
+	// FiPlayCircle,
+	FiSettings,
 	FiVolume2,
 	FiVolumeX,
 } from 'react-icons/fi';
@@ -34,7 +37,8 @@ const VideoController = ({
 	toggleFullScreen,
 }: Props) => {
 	return (
-		<div className='absolute bottom-0 left-0 right-0 px-1 py-2 z-20 opacity-0 group-hover/video-player-item:opacity-100 transition-opacity duration-300'>
+		<div className='absolute bottom-0 left-0 right-0 px-1 py-2 z-20 opacity-100 group-hover/video-player-item:opacity-100 transition-opacity duration-300'>
+			{/* Video SeekBar */}
 			<div className='flex items-center'>
 				<input
 					type='range'
@@ -45,8 +49,10 @@ const VideoController = ({
 					onChange={updateSeekBar}
 				/>
 			</div>
+			{/* Video Controller Information */}
 			<div className='mt-1 flex items-center justify-between gap-1.5'>
 				<div className='flex items-center gap-1.5'>
+					{/* Video Play/Pause */}
 					<button
 						type='button'
 						className='p-1 bg-transparent outline-none'
@@ -58,13 +64,13 @@ const VideoController = ({
 							<FiPause className='w-5 h-5 fill-white stroke-1' />
 						)}
 					</button>
-
+					{/* Video timestamps */}
 					<div className='flex items-center gap-1 text-white'>
 						<span>{formateTime(timeElapsed)}</span>
 						<span>/</span>
 						<span>{formateTime(duration)}</span>
 					</div>
-
+					{/* Video Volume */}
 					<div className='flex items-center gap-1 group/video-player-volume'>
 						<button
 							type='button'
@@ -94,9 +100,52 @@ const VideoController = ({
 						className='p-1 bg-transparent outline-none'
 						onClick={toggleFullScreen}
 					>
+						<FiSettings className='w-5 h-5 text-white stroke-2' />
+					</button>
+					<button
+						type='button'
+						className='p-1 bg-transparent outline-none'
+						onClick={toggleFullScreen}
+					>
 						<FiMaximize className='w-5 h-5 text-white stroke-2' />
 					</button>
 				</div>
+			</div>
+
+			{/* settings popup window gose here  */}
+			<div className='absolute -top-20 right-0 bg-black/80 rounded-xl'>
+				{/* <ul>
+					<li className='px-3.5 py-1.5 text-white flex items-center justify-between'>
+						<span className='flex items-center gap-1'>
+							<FiPlayCircle className='w-5 h-5 stroke-1' />
+							<span>Playback Speed</span>
+						</span>
+						<span className='flex items-center gap-1 text-xs'>
+							<span className='underline'>Normal</span>
+							<FiChevronRight className='w-5 h-5 stroke-1' />
+						</span>
+					</li>
+					<li className='px-3.5 py-1.5 text-white flex items-center justify-between'>
+						<span className='flex items-center gap-1'>
+							<FiPlayCircle className='w-5 h-5 stroke-1' />
+							<span>Subtitle</span>
+						</span>
+						<span className='flex items-center gap-1 text-xs'>
+							<span className='underline'>Off</span>
+							<FiChevronRight className='w-5 h-5 stroke-1' />
+						</span>
+					</li>
+					<li className='px-3.5 py-1.5 text-white flex items-center justify-between'>
+						<span className='flex items-center gap-1'>
+							<FiPlayCircle className='w-5 h-5 stroke-1' />
+							<span>Quality</span>
+						</span>
+						<span className='flex items-center gap-1 text-xs'>
+							<span className='underline'>Auto</span>
+							<FiChevronRight className='w-5 h-5 stroke-1' />
+						</span>
+					</li>
+				</ul> */}
 			</div>
 		</div>
 	);
