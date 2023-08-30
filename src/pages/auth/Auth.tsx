a79d3bbb7784a8b94e909c853656e18ec381e622
A B M Zubayer <zubayerabm47@gmail.com>
2023-08-30 12:35:25 +0600

    MODIFIED: redirect to home when user loggedIn

--- src/pages/auth/Auth.tsx
+++ src/pages/auth/Auth.tsx
@@ -1,4 +1,6 @@
 import { FC, useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
 import ForgetPassPage from './ForgetPassPage';
 import LoginPage from './LoginPage';
 import RegisterPage from './RegisterPage';
@@ -6,11 +8,15 @@ import RegisterPage from './RegisterPage';
 interface AuthProps { }
 
 const Auth: FC<AuthProps> = () => {
+    const { state: { isLoggedIn } } = useAuth();
     const [isLogin, setIsLogin] = useState(false);
     const [isForgetPass, setIsForgetPass] = useState(false);
 
     return (
         <>
+
+            {isLoggedIn && <Navigate to='/' />}
+
             {!isLogin ? (
                 <RegisterPage setIsLogin={setIsLogin} />
             ) : (
