a6fbf07539bd3e944614dec987b84f6fa35d59f1
A B M Zubayer <zubayerabm47@gmail.com>
2023-08-30 22:00:47 +0600

    FIX: opening modal on /auth route.

--- src/pages/auth/authModals/AuthModal.tsx
+++ src/pages/auth/authModals/AuthModal.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import ModalBox from '../../../components/ModalViews/ModalBox';
 import useModal from '../../../hooks/useModal';
 import ForgetPassPage from '../ForgetPassPage';
@@ -7,12 +8,19 @@ import LoginPage from '../LoginPage';
 const AuthModal = () => {
     const [isForgetPass, setIsForgetPass] = useState(false);
     const { dispatch: modalDispatch } = useModal();
+    const location = useLocation();
+
+    // would it be a good approach or not?
+    if (location.pathname === '/auth') {
+        modalDispatch({ type: 'UPDATE_AUTH_MODAL', payload: false })
+    }
 
     return (
         <ModalBox onClose={() => modalDispatch({ type: "UPDATE_AUTH_MODAL", payload: false })}
             classes="h-fit max-w-96 m-auto"
             overlyBg="bg-black/20"
         >
+
             {!isForgetPass ? (
                 <LoginPage setIsForgetPass={setIsForgetPass} />
             ) : (
