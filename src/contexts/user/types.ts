d2662bf3592bc459f723cb5d5b98aaadaca80ae9
A B M Zubayer <zubayerabm47@gmail.com>
2023-08-30 12:34:36 +0600

    MODIFIED: modify and add

--- src/contexts/user/types.ts
+++ src/contexts/user/types.ts
@@ -1,16 +1,16 @@
 export enum UserRight {
-	USER = 101,
-	MODER = 201,
-	ADMIN = 302,
+  USER = 101,
+  MODER = 201,
+  ADMIN = 302,
 }
 
 interface User {
-	id: number;
-	fullname: string;
-	username: string;
-	rights: UserRight;
-	active?: boolean;
-	unable?: boolean;
+  id: string;
+  username: string;
+  fullname: string;
+  email: string;
+  token: string;
+  avater: string | null;
 }
 
 // type UserPartial = {
@@ -20,28 +20,47 @@ interface User {
 //   unable?: boolean;
 // };
 
+type AuthErrorType = {
+  message?: string;
+
+  fullname?: string;
+  username?: string;
+  email?: string;
+  password?: string;
+};
+
 export interface UserStateType {
-	authLoading: boolean;
-	isLoggedIn: boolean;
-	user: User | null;
+  authError: AuthErrorType | null;
+  authLoading: boolean;
+  isLoggedIn: boolean;
+  user: User | null;
 }
 
 // #-------------REDUCER TYPES--------------
 type AUTH_LOADING = {
-	type: 'AUTH_LOADING';
-	payload?: boolean;
+  type: "AUTH_LOADING";
+  payload?: boolean;
+};
+type AUTH_ERROR = {
+  type: "AUTH_ERROR";
+  payload: AuthErrorType | null;
 };
 type SET_AUTH = {
-	type: 'SET_AUTH';
-	payload: User;
+  type: "SET_AUTH";
+  payload: User;
 };
 type REM_AUTH = {
-	type: 'REM_AUTH';
+  type: "REM_AUTH";
 };
 type UPDATE_USER = {
-	type: 'UPDATE_USER';
-	payload: User;
-	// payload: Partial<UserPartial>;
+  type: "UPDATE_USER";
+  payload: User;
+  // payload: Partial<UserPartial>;
 };
 
-export type UserActionType = AUTH_LOADING | SET_AUTH | REM_AUTH | UPDATE_USER;
+export type UserActionType =
+  | AUTH_ERROR
+  | AUTH_LOADING
+  | SET_AUTH
+  | REM_AUTH
+  | UPDATE_USER;
