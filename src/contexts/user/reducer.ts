6fe4d80c8662455e45874b185a6a588ab93e7f6f
A B M Zubayer <zubayerabm47@gmail.com>
2023-08-30 12:34:07 +0600

    ADDED: add auth errors

--- src/contexts/user/reducer.ts
+++ src/contexts/user/reducer.ts
@@ -1,43 +1,50 @@
-import { initUserState } from './state';
-import { UserActionType, UserStateType } from './types';
+import { initUserState } from "./state";
+import { UserActionType, UserStateType } from "./types";
 
 const userReducer = (
-	state: UserStateType,
-	action: UserActionType
+  state: UserStateType,
+  action: UserActionType
 ): UserStateType => {
-	switch (action.type) {
-		case 'AUTH_LOADING':
-			return {
-				...state,
-				authLoading: !action.payload || !state.authLoading,
-			};
+  switch (action.type) {
+    case "AUTH_LOADING":
+      return {
+        ...state,
+        authLoading: action.payload || !state.authLoading,
+      };
 
-		case 'SET_AUTH': {
-			const userInfo = action.payload;
-			return {
-				authLoading: false,
-				isLoggedIn: true,
-				user: {
-					...userInfo,
-				},
-			};
-		}
+    case "SET_AUTH": {
+      const userInfo = action.payload;
+      return {
+        authError: null,
+        authLoading: false,
+        isLoggedIn: true,
+        user: {
+          ...userInfo,
+        },
+      };
+    }
 
-		case 'UPDATE_USER':
-			return {
-				...state,
-				user: {
-					...state.user,
-					...action.payload,
-				},
-			};
+    case "UPDATE_USER":
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          ...action.payload,
+        },
+      };
 
-		case 'REM_AUTH':
-			return initUserState;
+    case "AUTH_ERROR":
+      return {
+        ...state,
+        authError: action.payload,
+      };
 
-		default:
-			return state;
-	}
+    case "REM_AUTH":
+      return initUserState;
+
+    default:
+      return state;
+  }
 };
 
 export default userReducer;
